b91afc69ff4d2af2e7904915250da69163c8ae67
Rano06 <104849491+Rano06@users.noreply.github.com>
2024-03-11 18:12:39 +0100

    Update MaClass-3.js

--- MaClass-3.js
+++ MaClass-3.js
@@ -215,7 +215,7 @@ function PrayTimes(method) {
 
 	// Default Parameters in Calculation Methods
 	defaultParams = {
-		maghrib: '0 min', midnight: 'Standard', imsak: 20 ,
+		maghrib: '0 min', midnight: 'Standard', imsak: 20
 
 	},
 
